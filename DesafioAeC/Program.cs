356a0df29fdf81be07142ef88d2685edab372c38
hiagor.freitas <hiagor.freitas@aec.com.br>
2023-06-08 10:43:35 -0300

    Commit Inicial da Estrutura do Projeto

---
+++ DesafioAeC/Program.cs
@@ -0,0 +1,14 @@
+﻿using Dominio.Infraestrutura.Interfaces.Servicos;
+using Dominio.Servicos;
+using Microsoft.Extensions.DependencyInjection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Injecao de dependencia
+        var serviceProvider = new ServiceCollection()
+                .AddSingleton<ISeleniumServices, SeleniumServices>()
+                .BuildServiceProvider();
+    }
+}
---
+++ Dominio/Infraestrutura/Interfaces/Servicos/ISeleniumServices.cs
@@ -0,0 +1,10 @@
+﻿using OpenQA.Selenium;
+
+namespace Dominio.Infraestrutura.Interfaces.Servicos
+{
+    public interface ISeleniumServices
+    {
+        public IWebDriver NavegarPagina(string url);
+        public IWebDriver PesquisaPagina(IWebDriver driver, string itemPesquisa);
+    }
+}
